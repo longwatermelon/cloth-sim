1508fe9aaad91e61863aef8b96dbb8a841241dec
longwatermelon <longwatermelon1@gmail.com>
2022-07-18 10:43:46 -0700

    spring gen

--- src/mesh.c
+++ src/mesh.c
@@ -8,13 +8,19 @@
 struct Mesh *mesh_alloc()
 {
     struct Mesh *m = malloc(sizeof(struct Mesh));
+    m->size = 10;
 
     m->verts = 0;
     m->nverts = 0;
     m->indices = 0;
     m->nindices = 0;
+    m->masses = 0;
+    m->nmasses = 0;
+    m->springs = 0;
+    m->nsprings = 0;
 
     mesh_construct(m);
+    mesh_gen_springs(m);
 
     glGenVertexArrays(1, &m->vao);
     glBindVertexArray(m->vao);
@@ -47,6 +53,8 @@ void mesh_free(struct Mesh *m)
 {
     free(m->verts);
     free(m->indices);
+    free(m->masses);
+    free(m->springs);
 
     glDeleteVertexArrays(1, &m->vao);
     glDeleteBuffers(1, &m->vb);
@@ -72,30 +80,33 @@ void mesh_render(struct Mesh *m, RenderInfo *ri)
 
 void mesh_construct(struct Mesh *m)
 {
-    for (int y = 0; y < 10; ++y)
+    for (int y = 0; y < m->size; ++y)
     {
-        for (int z = 0; z < 10; ++z)
+        for (int z = 0; z < m->size; ++z)
         {
             Vertex v = {
                 { 5.f, y, z },
-                { (float)(y * 10) / 100.f, 0.f, 1.f - (float)(z * 10) / 100.f }
+                { (float)(y * 10) / 100.f, 1.f - (float)(y * 10) / 100.f, 1.f - (float)(z * 10) / 100.f }
             };
 
             m->verts = realloc(m->verts, sizeof(Vertex) * ++m->nverts);
             m->verts[m->nverts - 1] = v;
 
+            m->masses = realloc(m->masses, sizeof(struct Mass) * ++m->nmasses);
+            m->masses[m->nmasses - 1] = (struct Mass){ 1.f, { 0.f, 0.f, 0.f }, &m->verts[m->nverts - 1] };
+
             if (y != 9 && z != 9)
             {
-                unsigned int i = y * 10 + z;
+                unsigned int i = y * m->size + z;
                 unsigned int ta[3], tb[3];
 
                 ta[0] = i;
-                ta[1] = i + 11;
-                ta[2] = i + 10;
+                ta[1] = i + m->size + 1;
+                ta[2] = i + m->size;
 
                 tb[0] = i;
                 tb[1] = i + 1;
-                tb[2] = i + 11;
+                tb[2] = i + m->size + 1;
 
                 size_t ia = m->nindices;
                 size_t ib = m->nindices + 3;
@@ -106,6 +117,50 @@ void mesh_construct(struct Mesh *m)
                 memcpy(m->indices + ia, ta, sizeof(unsigned int) * 3);
                 memcpy(m->indices + ib, tb, sizeof(unsigned int) * 3);
             }
+        }
+    }
+}
+
+
+void mesh_gen_springs(struct Mesh *m)
+{
+    // edges + crosses
+    int area = (m->size - 1) * (m->size - 1); // m->size counts vertices not squares
+    m->nsprings = (area * 2 + (m->size - 1) * 2) + (area * 2);
+    m->springs = malloc(sizeof(struct Spring) * m->nsprings);
+
+    size_t index = 0;
+
+    float k = 10.f;
+    float eq_len = 1.f;
+
+    // horizontal and vertical
+    for (size_t y = 0; y < m->size; ++y)
+    {
+        for (size_t z = 0; z < m->size - 1; ++z)
+        {
+            size_t mi = y * m->size + z;
+            m->springs[index++] = (struct Spring){ &m->masses[mi], &m->masses[mi + 1], k, eq_len };
+        }
+    }
+
+    for (size_t y = 0; y < m->size - 1; ++y)
+    {
+        for (size_t z = 0; z < m->size; ++z)
+        {
+            size_t mi = y * m->size + z;
+            m->springs[index++] = (struct Spring){ &m->masses[mi], &m->masses[mi + m->size], k, eq_len };
+        }
+    }
+
+    // diagonal
+    for (size_t y = 0; y < m->size - 1; ++y)
+    {
+        for (size_t z = 0; z < m->size - 1; ++z)
+        {
+            size_t mi = y * m->size + z;
+            m->springs[index++] = (struct Spring){ &m->masses[mi], &m->masses[mi + m->size + 1], k, eq_len };
+            m->springs[index++] = (struct Spring){ &m->masses[mi + m->size], &m->masses[mi + 1], k, eq_len };
         }
     }
 }
--- src/mesh.h
+++ src/mesh.h
@@ -9,10 +9,32 @@ typedef struct
     vec3 pos, col;
 } Vertex;
 
+struct Mass
+{
+    float mass;
+    vec3 vel;
+
+    Vertex *vert;
+};
+
+struct Spring
+{
+    struct Mass *ma, *mb;
+    float k, eq_len;
+};
+
 struct Mesh
 {
+    int size;
+
     Vertex *verts;
     size_t nverts;
+
+    struct Mass *masses;
+    size_t nmasses;
+
+    struct Spring *springs;
+    size_t nsprings;
 
     unsigned int *indices;
     size_t nindices;
@@ -26,6 +48,7 @@ void mesh_free(struct Mesh *m);
 void mesh_render(struct Mesh *m, RenderInfo *ri);
 
 void mesh_construct(struct Mesh *m);
+void mesh_gen_springs(struct Mesh *m);
 
 #endif
 
